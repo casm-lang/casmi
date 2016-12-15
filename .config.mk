99eccb7b9aa3dd833fb569af728f4128dee615ba
Philipp Paulweber <p.paulweber@gmail.com>
2016-12-15 09:55:10 +0100

    Building
    * fixed license text

--- .config.mk
+++ .config.mk
@@ -5,22 +5,22 @@
 #   Developed by: Florian Hahn
 #                 Philipp Paulweber
 #                 Emmanuel Pescosta
-#                 https://github.com/casm-lang/libcasm-fe
+#                 https://github.com/casm-lang/casmi
 #
-#   This file is part of libcasm-fe.
+#   This file is part of casmi.
 #
-#   libcasm-fe is free software: you can redistribute it and/or modify
+#   casmi is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
-#   libcasm-fe is distributed in the hope that it will be useful,
+#   casmi is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #   GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with libcasm-fe. If not, see <http://www.gnu.org/licenses/>.
+#   along with casmi. If not, see <http://www.gnu.org/licenses/>.
 #
 
 ifndef TARGET
