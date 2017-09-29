32ff0567776c02c4aa0e1680b1012b337755d72a
Philipp Paulweber <p.paulweber@gmail.com>
2017-09-29 17:55:33 +0200

    Passes
    * added missing symbol registration and type check pass

--- src/casmi.cpp
+++ src/casmi.cpp
@@ -179,8 +179,10 @@ int main( int argc, const char* argv[] )
         } );
 
     pm.add< libcasm_fe::AttributionPass >();
+    pm.add< libcasm_fe::SymbolRegistrationPass >();
     pm.add< libcasm_fe::SymbolResolverPass >();
     pm.add< libcasm_fe::TypeInferencePass >();
+    pm.add< libcasm_fe::TypeCheckPass >();
     pm.add< libcasm_fe::ConsistencyCheckPass >();
 
     pm.add< libcasm_fe::AstDumpDotPass >();
